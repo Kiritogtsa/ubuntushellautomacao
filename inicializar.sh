882fa9141dc3e6b09c0e3a5758678a02e18c117e
tiago <tenseishitarrakenseshita@gmail.com>
2024-04-12 18:25:22 -0300

    versao 2.0

--- inicializar.sh
+++ inicializar.sh
@@ -39,7 +39,11 @@ travas_apt() {
 instalacaodebarray=(
     "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
     "https://objects.githubusercontent.com/github-production-release-asset-2e65be/16408992/612a4416-1cc3-409b-b2f6-17761c1c8976?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240407%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240407T080410Z&X-Amz-Expires=300&X-Amz-Signature=da326590fc234fc1cd7cc3a1a34adf997eb35e3b048cc41d786a02a260b15913&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=16408992&response-content-disposition=attachment%3B%20filename%3Dnvim-linux64.tar.gz&response-content-type=application%2Foctet-stream"
+    https://codeload.github.com/dracula/tilix/zip/refs/heads/master
 )
+installdracula() {
+    wget 
+}
 limpeza() {
     atualizar
     sudo apt autoremove -y
@@ -56,6 +60,15 @@ instalardeb() {
         wget $url
     done
     sudo dpkg -i ./*.deb
+    if [ -e $HOME/.config/tilix/ ];then
+        if [ -e tilix-master.zip ]; then
+            unzip tilix-master.zip
+            cd tilix-master
+            cp Dracula.json $HOME/.conf/tilix/schemes
+        fi
+        xdg-mime default org.gnome.Terminal.desktop application/x-vnd.vte-terminal
+    fi
+    
 }
 aptinstall() {
     apks=("fonts-firacode" "curl" "git" "zsh")
